3d1c0c67f3401a13a3e650d2dd5f866c6fc688c2
liuhui <642404044@qq.com>
2017-07-20 23:55:12 +0800

    添加图片封装

--- app/src/main/java/com/lh/frameproj/ui/fragment2/order_working/OrderWorkingFragement.java
+++ app/src/main/java/com/lh/frameproj/ui/fragment2/order_working/OrderWorkingFragement.java
@@ -1,16 +1,26 @@
 package com.lh.frameproj.ui.fragment2.order_working;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.android.frameproj.library.util.imageloader.ImageLoaderUtil;
 import com.lh.frameproj.R;
 import com.lh.frameproj.ui.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by we-win on 2017/7/20.
  */
 
 public class OrderWorkingFragement extends BaseFragment {
+    @BindView(R.id.imageView)
+    ImageView mImageView;
+
     @Override
     public void initInjector() {
 
@@ -33,6 +43,15 @@ public class OrderWorkingFragement extends BaseFragment {
 
     @Override
     public void initData() {
+        ImageLoaderUtil.getInstance().loadImage("http://image.sports.baofeng.com/25a3dbb0c99c5e48e52e60941ed230be", R.mipmap.ic_launcher, mImageView);
 
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        ButterKnife.bind(this, rootView);
+        return rootView;
     }
 }
---
+++ library/src/main/java/com/android/frameproj/library/util/imageloader/BaseImageLoaderStrategy.java
@@ -0,0 +1,64 @@
+package com.android.frameproj.library.util.imageloader;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.android.frameproj.library.util.imageloader.glideprogress.ProgressLoadListener;
+import com.android.frameproj.library.util.imageloader.listener.ImageSaveListener;
+import com.android.frameproj.library.util.imageloader.listener.SourceReadyListener;
+
+
+/**
+ * Created by soulrelay on 2016/10/11.
+ * Class Note:
+ * abstract class/interface defined to load image
+ * (Strategy Pattern used here)
+ * 图片加载的基础接口
+ * 当封装的方法参数比较少时可以下面这样进行抽象，如果需要传递的参数较多，可以考虑使用建造者模式
+ */
+public interface BaseImageLoaderStrategy {
+    //正常加载图片 无占位图
+    void loadImage(String url, ImageView imageView);
+
+    //这里的context指定为ApplicationContext
+    void loadImageWithAppCxt(String url, ImageView imageView);
+
+    void loadImage(String url, int placeholder, ImageView imageView);
+
+    void loadImage(Context context, String url, int placeholder, ImageView imageView);
+
+    //加载圆形图片
+    void loadCircleImage(String url, int placeholder, ImageView imageView);
+
+    //加载圆形图片，支持设置边框的宽度和颜色
+    void loadCircleBorderImage(String url, int placeholder, ImageView imageView, float borderWidth, int borderColor);
+
+    void loadCircleBorderImage(String url, int placeholder, ImageView imageView, float borderWidth, int borderColor, int heightPx, int widthPx);
+
+    // 针对于GIF图片的特殊加载
+    void loadGifImage(String url, int placeholder, ImageView imageView);
+
+    // 加载图片的进度回调
+    void loadImageWithProgress(String url, ImageView imageView, ProgressLoadListener listener);
+
+    void loadImageWithPrepareCall(String url, ImageView imageView, int placeholder, SourceReadyListener listener);
+
+    void loadGifWithPrepareCall(String url, ImageView imageView, SourceReadyListener listener);
+
+    //清除硬盘缓存
+    void clearImageDiskCache(final Context context);
+
+    //清除内存缓存
+    void clearImageMemoryCache(Context context);
+
+    //根据不同的内存状态，来响应不同的内存释放策略
+    void trimMemory(Context context, int level);
+
+    //获取缓存大小
+    String getCacheSize(Context context);
+
+    // 实现图片的本地自定义保存功能
+    void saveImage(Context context, String url, String savePath, String saveFileName, ImageSaveListener listener);
+
+    // TODO 其它特殊需求自己封装，最好不要破坏策略模式的整体结构
+}
